f748f8d9c523b7925f0b192fb5512eb90ce1360c
coderbhawesh <20cs3021@rgipt.ac.in>
2024-08-25 21:10:39 +0530

    delete content of calc.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,3 @@
 Calculator functions
 """
 
-def add(x,y):
-    """Add x and y and result result."""
-    return x+y
-
-def subtract(x,y):
-    """Subtract x and y and return result"""
-    return y-x
